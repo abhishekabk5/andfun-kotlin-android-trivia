32756db85b0376650948724aea54e7e967550ff6
Arlak <kalraabhishek98@gmail.com>
2019-04-18 00:00:07 +0530

    Followed exercises as described in the lesson

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -16,24 +16,16 @@
 
 package com.example.android.navigation
 
-import android.content.ActivityNotFoundException
 import android.content.Intent
-import androidx.databinding.DataBindingUtil
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.core.app.ShareCompat
-import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
+import androidx.core.app.ShareCompat
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameWonBinding
-import android.content.pm.ResolveInfo
-import android.content.pm.PackageManager
-
 
 
 class GameWonFragment : Fragment() {
@@ -42,38 +34,39 @@ class GameWonFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_won, container, false)
+        var args = GameWonFragmentArgs.fromBundle(arguments!!)
+
         binding.nextMatchButton.setOnClickListener { view: View ->
-            view.findNavController().navigate(
-                    GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
+            view.findNavController().navigate(GameWonFragmentDirections
+                        .actionGameWonFragmentToGameFragment(args.numCorrect))
         }
+
         setHasOptionsMenu(true)
         return binding.root
     }
 
-    private fun getShareIntent() : Intent {
-        val args = GameWonFragmentArgs.fromBundle(arguments!!)
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.winner_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+
+        if(null == getSharedIntent().resolveActivity(activity!!.packageManager))
+            menu.findItem(R.id.share)?.isVisible = false
+    }
+
+    private fun getSharedIntent() : Intent {
+        var args = GameWonFragmentArgs.fromBundle(arguments!!)
         return ShareCompat.IntentBuilder.from(activity)
-                .setText(getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+                .setText(getString(R.string.share_success_text, args.numCorrect))
                 .setType("text/plain")
                 .intent
     }
 
     private fun shareSuccess() {
-        startActivity(getShareIntent())
+        startActivity(getSharedIntent())
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
-        super.onCreateOptionsMenu(menu, inflater)
-        inflater?.inflate(R.menu.winner_menu, menu)
-        // check if the activity resolves
-        if (null == getShareIntent().resolveActivity(activity!!.packageManager)) {
-            // hide the menu item if it doesn't resolve
-            menu?.findItem(R.id.share)?.setVisible(false)
-        }
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        when (item!!.itemId) {
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
             R.id.share -> shareSuccess()
         }
         return super.onOptionsItemSelected(item)
